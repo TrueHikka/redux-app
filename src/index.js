0b59b2c66bce6edad0ca3db8eaabcea7ef90e21c
Vitaly <vitalyrubis@gmail.com>
2022-10-28 17:12:44 +0300

    Redux Toolkit. Configure Store and Providing Store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import {titleChanged, taskDeleted, completeTask, getTasks} from "./store/task"
 import configureStore from './store/store';
+import { Provider } from 'react-redux';
 
 const store = configureStore()
 
@@ -49,7 +50,9 @@ const App = () => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+	<Provider store={store}>
     <App />
+	</Provider>
   </React.StrictMode>
 );
 
