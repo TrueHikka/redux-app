85355eadb1c507ff270b88c20447dc3681089add
Vitaly <vitalyrubis@gmail.com>
2022-10-28 18:00:01 +0300

    Добавляем загрузку и обработку ошибок

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import { Provider, useDispatch, useSelector } from 'react-redux';
 const store = configureStore()
 
 const App = () => {
-	const state = useSelector((state) => state)
-
+	const state = useSelector((state) => state.entities)
+	const isLoading = useSelector(state => state.isLoading)
+	const error = useSelector(state => state.error )
 	const dispatch = useDispatch()
 
 	const changeTitle = (taskId) => {
@@ -22,6 +23,13 @@ const App = () => {
 	useEffect(() => {
 		dispatch(getTasks())
 	}, [])
+
+	if(isLoading) {
+		return <h1>Loading...</h1>
+	}
+	if(error) {
+		return <p>{error}</p>
+	}
 
 	return <>
 	<h1>App</h1> 
